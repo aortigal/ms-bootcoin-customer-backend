415895f77625965d818a70b3e075da78c85fd67d
Amado David Ortiz Gallardo <aortigal@emeal.nttdata.com>
2022-08-29 19:16:35 -0500

    add estructure base

---
+++ src/main/java/com/bank/msbootcoincustomerbackend/MsBootcoinCustomerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bank.msbootcoincustomerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBootcoinCustomerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBootcoinCustomerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/msbootcoincustomerbackend/MsBootcoinCustomerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.msbootcoincustomerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBootcoinCustomerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
